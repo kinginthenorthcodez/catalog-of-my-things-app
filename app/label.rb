fcc8c5dbfe135b406324ee4c21d74f6f0845a23e
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-08-09 19:48:32 -0300

    Fix linter errors

--- app/label.rb
+++ app/label.rb
@@ -1,14 +1,15 @@
 require_relative './item'
 
 class Label
-  def initialize (color, title)
+  def initialize(color, title)
     @color = color
     @title = title
     @items = []
     @id = rand(1..1000)
   end
-  def add_item (item)
-    items << item unless items.include? (item)
+
+  def add_item(item)
+    items << item unless items.include?(item)
     item.label = self
   end
 end
