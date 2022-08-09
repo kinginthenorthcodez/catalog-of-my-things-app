dded6bc160c8daaa9ebaaca1eabc7c1f880730f1
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-08-09 18:56:28 -0300

    Fix linter errors

--- app/book.rb
+++ app/book.rb
@@ -1,12 +1,16 @@
-require_relative'./item.rb'
+require_relative './item'
 
 class Book < Item
-  def initialize(publisher,cover_state)
+  def initialize(publisher, cover_state, publish_date, id)
+    super(publish_date, id)
     @publisher = publisher
     @cover_state = cover_state
+    @publish_date = publish_date
+    @id = id
   end
+
   def can_be_archived?()
     true if can_be_archived || @cover_state == 'bad'
     false
   end
-  end
+end
