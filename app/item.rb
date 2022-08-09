7de497db5e1ed7fbf2ac0a229f39133a1931b547
gedewon <gedewon.hayle.9@gmail.com>
2022-08-09 19:44:15 +0300

    Create User Interface

--- app/item.rb
+++ app/item.rb
@@ -1,26 +1,10 @@
 class Item
- attr_reader :source, :lable, :genre, :author
- 
+  attr_accessor :source, :lable, :genre, :author
+
   def initialize(publish_date, archived)
     @id = Random.rand(1..1000)
     @publish_date = publish_date
     @archived = archived
-  end
-
-  def source=(source)
-    @source = source
-  end
-
-  def lable=(lable)
-    @lable = lable
-  end
-
-  def genre=(genre)
-    @genre = genre
-  end
-
-  def author=(author)
-    @author = author
   end
 
   def move_to_archive
@@ -31,6 +15,7 @@ class Item
 
   def can_be_archived?
     return true if @publish_date > 10
+
     false
   end
 end
