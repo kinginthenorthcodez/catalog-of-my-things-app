1415e0dcbdbb6cdf592a91f7e8c443942f6265fe
kinginthenorthcodez <kinginthenorthcodez@gmail.com>
2022-08-10 23:16:07 +0530

    Team #3 tasks
    Add game class
    Add Author class
    implement all methods in classes and properties
    Add unit tests for class methods and properties

---
+++ spec/author_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../app/author'
+
+
+describe "Author" do
+  context " Author add item method  test " do
+    it "Should add item to items" do 
+      author = Author.new('Isaac', 'Maqueen')
+      mock = double('item')
+      allow(author).to receive(:add_item).and_return('item added')
+      expect(author.add_item(mock)).to eq 'item added'
+    end
+  end
+  context " Author construct test " do
+    it "Should should have first and last name" do 
+      author = Author.new('Isaac', 'Maqueen')
+      expect(author.first_name).to eq 'Isaac'
+      expect(author.last_name).to eq 'Maqueen'
+    end
+  end
+end
