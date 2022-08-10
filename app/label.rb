162d7f90b3cead778bf25700fb5a522d3d083acd
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-08-10 03:31:33 -0300

    Add label test file

--- app/label.rb
+++ app/label.rb
@@ -1,6 +1,8 @@
 require_relative './item'
 
 class Label
+  attr_accessor :color, :title, :items
+
   def initialize(color, title)
     @color = color
     @title = title
--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -1,1 +1,17 @@
 require_relative'../app/label.rb'
+
+describe Label do
+  before(:all) do
+    @label = Label.new('black', 'new')
+  end
+  it 'the label constructor starts correctly' do
+    expect(@label.title).to eq 'new'
+    expect(@label.color).to eq 'black'
+  end
+  it 'add_item method works fine' do
+    @book = Book.new('publisher1','bad',12/12/2020)
+    @label.add_item(@book)
+    expect(@label.items.length).to eq 1
+    expect(@book.label).to eq @label
+    end
+end
