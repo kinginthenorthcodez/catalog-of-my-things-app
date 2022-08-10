9ebe5c52fef3a2195638f099f92703bbe59ecfd9
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-08-10 22:54:42 +0000

    Implement the can_be_archived method

--- app/music.rb
+++ app/music.rb
@@ -1,8 +1,13 @@
 class MusicAlbum
+
+   attr_accessor :on_spotify
+
    def initialize(on_spotify)
     @on_spotify = on_spotify
    end
 
    def can_be_archived?
+      true if super() && @on_spotify = true
+      false
    end
 end
