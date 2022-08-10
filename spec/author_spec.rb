e2cb6c6e80fab5cd5c2365d1df72ced9361bf28b
kinginthenorthcodez <kinginthenorthcodez@gmail.com>
2022-08-10 23:26:02 +0530

    Fix linter errors

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,17 +1,16 @@
 require_relative '../app/author'
 
-
-describe "Author" do
-  context " Author add item method  test " do
-    it "Should add item to items" do 
+describe 'Author' do
+  context ' Author add item method  test ' do
+    it 'Should add item to items' do
       author = Author.new('Isaac', 'Maqueen')
       mock = double('item')
       allow(author).to receive(:add_item).and_return('item added')
       expect(author.add_item(mock)).to eq 'item added'
     end
   end
-  context " Author construct test " do
-    it "Should should have first and last name" do 
+  context ' Author construct test ' do
+    it 'Should should have first and last name' do
       author = Author.new('Isaac', 'Maqueen')
       expect(author.first_name).to eq 'Isaac'
       expect(author.last_name).to eq 'Maqueen'
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,14 +1,14 @@
 require_relative '../app/game'
 
-describe "Game" do
-  context " Game method achieved test " do
-    it "Should return true if super && last_played_at > 2" do 
+describe 'Game' do
+  context ' Game method achieved test ' do
+    it 'Should return true if super && last_played_at > 2' do
       game = Game.new('player 1', 4, 12, true)
       expect(game.move_to_archive).to be true
     end
   end
-  context " Game construct test " do
-    it "Should should add player" do 
+  context ' Game construct test ' do
+    it 'Should should add player' do
       game = Game.new('player 1', 4, 12, true)
       expect(game.multplayer).to eq 'player 1'
     end
