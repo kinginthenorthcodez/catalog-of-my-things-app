9f539444f724929ee7cdd5cb13d7b80b3635e5bc
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-08-10 05:04:53 -0300

    Fix linter errors

--- app/book.rb
+++ app/book.rb
@@ -1,14 +1,16 @@
-require_relative './item'
+require_relrequire_relative './item'
 
 class Book < Item
   attr_accessor :publisher, :cover_state
 
+  # rubocop:disable Metrics/ParameterLists
   def initialize(publisher, cover_state, publish_date, author = nil, label = nil, genre = nil, source = nil)
     super(publish_date, author, label, genre, source)
     @publisher = publisher
     @cover_state = cover_state
   end
 
+  # rubocop:enable Metrics/ParameterLists
   def can_be_archived?
     return true if super() || @cover_state == 'bad'
 
