fbc4769631f50606e52d7cbc1274996aaf355293
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-08-10 03:37:21 -0300

    Fix linter errors

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -1,4 +1,4 @@
-require_relative'../app/label.rb'
+require_relative '../app/label'
 
 describe Label do
   before(:all) do
@@ -9,9 +9,9 @@ describe Label do
     expect(@label.color).to eq 'black'
   end
   it 'add_item method works fine' do
-    @book = Book.new('publisher1','bad',12/12/2020)
+    @book = Book.new('publisher1', 'bad', 12 / 12 / 2020)
     @label.add_item(@book)
     expect(@label.items.length).to eq 1
     expect(@book.label).to eq @label
-    end
+  end
 end
