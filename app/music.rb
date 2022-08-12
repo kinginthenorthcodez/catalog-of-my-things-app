e37ec37f4ca8514d4dc5b508a7560986d939e2df
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-08-12 12:53:48 +0000

    Write test for music class

--- app/music.rb
+++ app/music.rb
@@ -1,12 +1,17 @@
-class MusicAlbum
-  attr_accessor :on_spotify
+require_relative './item'
 
-  def initialize(on_spotify)
+class MusicAlbum < Item
+  attr_accessor :on_spotify, :archived, :publish_date
+
+  def initialize(on_spotify, publish_date, archived)
+    super(publish_date, archived)
     @on_spotify = on_spotify
+    @publish_date = publish_date
+    @archived = archived
   end
 
   def can_be_archived?
-    true if super() && @on_spotify == true
+    return true if super() && @on_spotify == true
     false
   end
 end
--- spec/music_spec.rb
+++ spec/music_spec.rb
@@ -1,2 +1,19 @@
 require_relative '../app/music'
 
+describe MusicAlbum do
+  before(:all) do
+    @music = MusicAlbum.new(true, 12 / 12 / 2022, false )
+  end
+
+  it 'Create instance of music class correctly' do
+    expect(@music.on_spotify).to eq true
+    expect(@music.publish_date).to eq 12 / 12 / 2022
+    expect(@music.archived).to eq false
+  end
+
+  it 'can_be_archived should return the correct bool' do
+     expect(@music.can_be_archived?).to eq false
+  end
+
+ 
+end
